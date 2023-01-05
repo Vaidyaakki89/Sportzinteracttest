5aa88f34c5076288c121c28839e01d1054818c7f
Akshay Vaidya <akshay@version-next.com>
2023-01-05 20:27:28 +0530

    Initial Commit

---
+++ sportzinteracttest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sportzinteracttest
+//
+//  Created by AKSHAY VAIDYA on 15/11/2022 Saka.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
